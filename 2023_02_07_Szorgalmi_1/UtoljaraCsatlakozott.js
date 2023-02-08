d1e120eeaacf0852738b33703389d87f78f37d62
thomassgithub <alienpo.ware@gmail.com>
2023-02-08 23:33:39 +0100

    Európai_Unio_Szorgalmi

---
+++ 2023_02_07_Szorgalmi_1/UtoljaraCsatlakozott.js
@@ -0,0 +1,130 @@
+<script>
+const EuropaiUnio = [{
+        orszag: "Ausztria",
+        csatlakozas: "1995.01.01"
+    },
+    {
+        orszag: "Belgium",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Bulgária",
+        csatlakozas: "2007.01.01"
+    },
+    {
+        orszag: "Ciprus",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Csehország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Dánia",
+        csatlakozas: "1973.01.01"
+    },
+    {
+        orszag: "Egyesült Királyság",
+        csatlakozas: "1973.01.01"
+    },
+    {
+        orszag: "Észtország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Finnország",
+        csatlakozas: "1995.01.01"
+    },
+    {
+        orszag: "Franciaország",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Görögország",
+        csatlakozas: "1981.01.01"
+    },
+    {
+        orszag: "Hollandia",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Horvátország",
+        csatlakozas: "2013.07.01"
+    },
+    {
+        orszag: "Írország",
+        csatlakozas: "1973.01.01"
+    },
+    {
+        orszag: "Lengyelország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Lettország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Litvánia",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Luxemburg",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Magyarország",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Málta",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Németország",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Olaszország",
+        csatlakozas: "1958.01.01"
+    },
+    {
+        orszag: "Portugália",
+        csatlakozas: "1986.01.01"
+    },
+    {
+        orszag: "Románia",
+        csatlakozas: "2007.01.01"
+    },
+    {
+        orszag: "Spanyolország",
+        csatlakozas: "1986.01.01"
+    },
+    {
+        orszag: "Svédország",
+        csatlakozas: "1995.01.01"
+    },
+    {
+        orszag: "Szlovákia",
+        csatlakozas: "2004.05.01"
+    },
+    {
+        orszag: "Szlovénia",
+        csatlakozas: "2004.05.01"
+    }
+]
+let CsatlakozottEvszam=[];
+for(let i=0;i<EuropaiUnio.length;i++){
+	CsatlakozottEvszam.push(EuropaiUnio[i].csatlakozas.substring(0,4));
+}
+let LegutobbCsatlakozott=CsatlakozottEvszam[0];
+for(let i=1;i<CsatlakozottEvszam.length;i++){
+	if(CsatlakozottEvszam[i]>LegutobbCsatlakozott){
+    	LegutobbCsatlakozott=CsatlakozottEvszam[i];
+    }
+}
+for(let i=0;i<EuropaiUnio.length;i++){
+	if(EuropaiUnio[i].csatlakozas.substring(0,4)==LegutobbCsatlakozott){
+    	document.write(`Az Európai Unióhoz legutoljára ${EuropaiUnio[i].orszag} csatlakozott ${LegutobbCsatlakozott}-ban`);
+    }
+}
+</script>
